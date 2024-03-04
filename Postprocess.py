1917ba2a699baf7f96aa7313fc8685c4d9797157
Mike Kryjak <mikekryjak@gmail.com>
2024-03-04 14:49:14 +0000

    Add post-processing function

--- Postprocess.py
+++ Postprocess.py
@@ -123,3 +123,87 @@ def get_band_widths(d, o, cvar, size = 0.05):
     x = spol_interp(c_grid)
             
     return x, band_widths
+
+
+
+def plot_morph_results(profiles, 
+                       store, 
+                       colors = ["teal", "darkorange"], 
+                       xlabel = "Profile change",
+                       show_47 = False,
+                       sens = True):
+
+    fig, axes = plt.subplots(2,3, dpi = 110, figsize = (12,8))
+
+    
+    thresholds = np.array([store[x]["threshold"] for x in profiles])
+    windows = np.array([store[x]["window_ratio"] for x in profiles]) 
+    L = np.array([profiles[x].get_connection_length() for x in profiles])
+    BxBt = np.array([profiles[x].get_total_flux_expansion() for x in profiles])
+    frac_gradB = np.array([profiles[x].get_average_frac_gradB() for x in profiles])
+    avgB_ratio = np.array([profiles[x].get_average_B_ratio() for x in profiles])
+
+    if sens:
+        target_sens = np.array([get_sensitivity(store[x]["crel_trim"], store[x]["SpolPlot"], fluctuation=1.05, location=0.0) for x in profiles])
+
+    L_base = L[0]
+    BxBt_base = BxBt[0]
+    threshold_base = thresholds[0]
+    window_base = windows[0]
+    avgB_ratio_base = avgB_ratio[0]
+        
+    threshcalc = (BxBt/BxBt_base)**(-1) * (L/L_base)**(-2/7) * (avgB_ratio/avgB_ratio_base)**(2/7)
+    threshcalc2 = (BxBt/BxBt_base)**(-1) * (L/L_base)**(-4/7) * (avgB_ratio/avgB_ratio_base)**(2/7)
+
+    windowcalc = (BxBt/BxBt_base)**(1) * (L/L_base)**(2/7) * (avgB_ratio/avgB_ratio_base)**(-2/7)
+    windowcalc2 = (BxBt/BxBt_base)**(1) * (L/L_base)**(4/7) * (avgB_ratio/avgB_ratio_base)**(-2/7)
+
+    index = list(profiles.keys())
+    index /= index[0]
+
+    # index = np.linspace(0,1,5)+i
+    axes[0,0].set_title("Connection length")
+    axes[0,0].plot(index, L/L_base, marker = "o")
+
+    axes[0,1].set_title(r"Total flux expansion $\frac{B_{X}}{B_{t}}$")
+    axes[0,1].plot(index, BxBt/BxBt_base, marker = "o")
+
+    axes[0,2].set_title(r"Average $(\frac{1}{B}) \frac{dB}{ds_{pol}}$ below X-point")
+    axes[0,2].plot(index, frac_gradB, marker = "o")
+
+    axes[1,0].set_title(r"Detachment threshold")
+    axes[1,0].plot(index, thresholds/threshold_base, marker = "o")
+
+    if show_47:
+        label_27 = "Analytical, $L^{2/7}$"
+    else:
+        label_27 = "Analytical"
+    axes[1,0].plot(index, threshcalc, color = "darkslategrey", ls = "--", label = label_27, zorder = 100)
+    
+    if show_47:
+        axes[1,0].plot(index, threshcalc2, color = "darkslategrey", ls = ":", label = "Analytical, $L^{4/7}$", zorder = 100)
+    axes[1,0].legend(fontsize = "x-small")
+
+    axes[1,1].set_title(r"Detachment window")
+    axes[1,1].plot(index, windows/window_base, marker = "o")
+    axes[1,1].plot(index, windowcalc, color = "darkslategrey", ls = "--", label = label_27, zorder = 100)
+    
+    if show_47:
+        axes[1,1].plot(index, windowcalc2, color = "darkslategrey", ls = ":", label = "Analytical, $L^{4/7}$", zorder = 100)
+    axes[1,1].legend(fontsize = "x-small")
+
+
+    axes[1,2].set_title(r"10% Sensitivity @ target")
+    
+    if sens: 
+        axes[1,2].plot(index, target_sens, marker = "o")
+
+    for ax in axes.flatten():
+        ax.set_xlabel(xlabel)
+
+    for ax in [axes[0,0], axes[0,1], axes[1,0], axes[1,1]]:
+        ax.set_ylabel("Relative change")
+        
+    axes[0,2].set_ylabel(r"$(\frac{1}{B}) \frac{dB}{ds_{pol}}$")
+    axes[1,2].set_ylabel("$S_{front, final} \ /\  S_{pol, x}$")
+    fig.tight_layout()
