f54d06f02bfb5ffa89aa35d92d9976c1f81919aa
Mike Kryjak <mikekryjak@gmail.com>
2024-03-07 19:13:01 +0000

    Add temperature and q profiles to output

--- LRBv21.py
+++ LRBv21.py
@@ -387,6 +387,8 @@ def LRBv21(constants,radios,d,SparRange,
                 Qrad.append(((si.nu0**2*st.Tu**2)/Tf**2)*si.cz0*si.Lfunc(Tf))
             
         output["Rprofiles"].append(Qrad)
+        output["Qprofiles"].append(st.q)
+        output["Tprofiles"].append(st.T)
         output["logs"].append(st.log)
         
     """------COLLECT RESULTS------"""
