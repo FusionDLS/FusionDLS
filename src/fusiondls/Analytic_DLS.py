b45384c6022050026ad7c6500215f712c6525f2a
Peter Hill <peter.hill@york.ac.uk>
2024-06-03 10:41:53 +0100

    Apply black

--- src/fusiondls/Analytic_DLS.py
+++ src/fusiondls/Analytic_DLS.py
@@ -7,7 +7,8 @@ from scipy import interpolate
 from .AnalyticCoolingCurves import LfuncN
 from .unpackConfigurations import unpackConfiguration
 
-def CfInt(spar, B_field, sx, L,sh = 0,kappa1=2500):
+
+def CfInt(spar, B_field, sx, L, sh=0, kappa1=2500):
     """
     spar: array, m
         Array of S parallel
@@ -18,35 +19,40 @@ def CfInt(spar, B_field, sx, L,sh = 0,kappa1=2500):
     L: float, m
         Connection length in  the parallel
     sh: float, m
-        Parallel front position 
+        Parallel front position
     kappa1: float, W/m^2/K^7/2
         Electron thermal conductivity
-    
+
     """
     """function which returns the control parameter required for a detachment front at parallel position sh"""
-    B_field = interpolate.interp1d(spar,B_field,kind='cubic',fill_value="extrapolate")
-    #calculate Tu/qpll**(2/7) by integrating over heat flux density
-    Tu = quad(integrand,sh,sx,args = (sx, L, B_field),epsabs = 0.0000000000000000001)[0]
-    if sx<L:
-        Tu = Tu+quad(integrand2,sx,L,args = (sx, L, B_field),epsabs = 0.0000000000000000001)[0]
-    Tu = (Tu*7/(2*kappa1))**(-2/7)
-    #account for flux expansion effects on heat flux density
-    Cf = Tu*B_field(sh)/B_field(sx)
-    #account for constants related to total impurity radiation
+    B_field = interpolate.interp1d(
+        spar, B_field, kind="cubic", fill_value="extrapolate"
+    )
+    # calculate Tu/qpll**(2/7) by integrating over heat flux density
+    Tu = quad(integrand, sh, sx, args=(sx, L, B_field), epsabs=0.0000000000000000001)[0]
+    if sx < L:
+        Tu = (
+            Tu
+            + quad(
+                integrand2, sx, L, args=(sx, L, B_field), epsabs=0.0000000000000000001
+            )[0]
+        )
+    Tu = (Tu * 7 / (2 * kappa1)) ** (-2 / 7)
+    # account for flux expansion effects on heat flux density
+    Cf = Tu * B_field(sh) / B_field(sx)
+    # account for constants related to total impurity radiation
     Q = []
-    T = np.linspace(0,100,1000)
+    T = np.linspace(0, 100, 1000)
     for t in T:
         Q.append(LfuncN(t))
-    C0 = (2*kappa1*trapz(Q*T**(1/2),T))**(-1/2)
-    Cf = Cf*C0
+    C0 = (2 * kappa1 * trapz(Q * T ** (1 / 2), T)) ** (-1 / 2)
+    Cf = Cf * C0
     return Cf
 
 
-def integrand(s,sx, L, B_field):
-        return (B_field(s)/B_field(sx))
-
-def integrand2(s,sx, L, B_field):
-        return  (L-s)*(B_field(s)/B_field(sx))/(L-sx)
-    
+def integrand(s, sx, L, B_field):
+    return B_field(s) / B_field(sx)
 
 
+def integrand2(s, sx, L, B_field):
+    return (L - s) * (B_field(s) / B_field(sx)) / (L - sx)
--- src/fusiondls/unpackConfigurationsMK.py
+++ src/fusiondls/unpackConfigurationsMK.py
@@ -6,14 +6,17 @@ from collections import defaultdict
 
 from .Profile import Profile
 
-def unpackConfigurationMK(File, 
-                          Type, 
-                          polModulator = 1, 
-                          sepadd = 0, 
-                          resolution = 300, 
-                          convention = "target_to_midplane", 
-                          diagnostic_plot = False,
-                          absolute_B = True):
+
+def unpackConfigurationMK(
+    File,
+    Type,
+    polModulator=1,
+    sepadd=0,
+    resolution=300,
+    convention="target_to_midplane",
+    diagnostic_plot=False,
+    absolute_B=True,
+):
     """
     Extract interpolated variables along the SOL for connected double null configurations
     File = balance file path
@@ -23,7 +26,7 @@ def unpackConfigurationMK(File,
     convention: target_to_midplane has target at s=0, midplane_to_target has midplane at s=0
     diagnostic_plot: plot a figure for a visual check
     absolute_B: return Bpol and Btot as absolute values
-    
+
     Outputs:
     Bpol: Poloidal B field
     Btot: Total B field
@@ -43,105 +46,103 @@ def unpackConfigurationMK(File,
 
     """------DATA EXTRACTION"""
     rootgrp = Dataset(File, "r", format="NETCDF4")
-    sep = rootgrp['jsep'][0] # separatrix cell ring
-    sep = sep + sepadd # select cell ring to work on
-    bb = rootgrp["bb"] # B vector array
+    sep = rootgrp["jsep"][0]  # separatrix cell ring
+    sep = sep + sepadd  # select cell ring to work on
+    bb = rootgrp["bb"]  # B vector array
 
-    full = dict() # dictionary to store parameters over full SOL ring
+    full = dict()  # dictionary to store parameters over full SOL ring
 
-    full["Bpol"] = bb[0][sep]*polModulator
+    full["Bpol"] = bb[0][sep] * polModulator
 
-    # bb[A] returns B components x, y, z and B magnitude for 
+    # bb[A] returns B components x, y, z and B magnitude for
     # A = 0, 1, 2 and 3 respectively for each cell centre in 2D grid
-    full["Btot"] = bb[3][sep] # Mistake in Cyd's version. [3] is the total field
-    
+    full["Btot"] = bb[3][sep]  # Mistake in Cyd's version. [3] is the total field
+
     # unpack dimensions of super-x
     # both r and z have the same shape as bb
-    r = rootgrp['crx'] # corner radial coordinate (m)
-    z = rootgrp['cry'] # corner vertical coordinate (m)
+    r = rootgrp["crx"]  # corner radial coordinate (m)
+    z = rootgrp["cry"]  # corner vertical coordinate (m)
 
     # ring cell centres
-    full["R"] = np.mean([r[0][sep], r[1][sep], r[2][sep], r[3][sep]], axis = 0)
-    full["Z"] = np.mean([z[0][sep], z[1][sep], z[2][sep], z[3][sep]], axis = 0)
+    full["R"] = np.mean([r[0][sep], r[1][sep], r[2][sep], r[3][sep]], axis=0)
+    full["Z"] = np.mean([z[0][sep], z[1][sep], z[2][sep], z[3][sep]], axis=0)
 
     # Entire grid cell centres
-    Zs = np.mean([z[0], z[1], z[2], z[3]], axis = 0)
-    Rs = np.mean([r[0], r[1], r[2], r[3]], axis = 0)
-    
+    Zs = np.mean([z[0], z[1], z[2], z[3]], axis=0)
+    Rs = np.mean([r[0], r[1], r[2], r[3]], axis=0)
+
     len_R = len(full["R"])
 
     """------XPOINT, EDGE, MIDPLANE LOCATIONS"""
     # Find the X-points, edges and midpoints
     # SOLPS draws grid clockwise from inner lower target
     # Points of gradient flipping represent features of interest
-    
+
     gradR = np.gradient(full["R"])
-    
+
     reversals = []
     for i in range(1, len(gradR)):
-        if np.sign(gradR[i-1]) != np.sign(gradR[i]):
+        if np.sign(gradR[i - 1]) != np.sign(gradR[i]):
             reversals.append(i)
-    
-    omp = reversals[-2] # outer midplane
-    imp = reversals[1] # inner midplane
+
+    omp = reversals[-2]  # outer midplane
+    imp = reversals[1]  # inner midplane
     xpoint = dict()
     target = dict()
     sol = defaultdict(dict)
 
-    xpoint["il"] = reversals[0] # inner lower xpoint
-    xpoint["iu"] = reversals[2] # outer upper xpoint
-    xpoint["ol"] = reversals[-1]-1 # outer lower xpoint
-    xpoint["ou"] = reversals[-3] # outer upper xpoint
+    xpoint["il"] = reversals[0]  # inner lower xpoint
+    xpoint["iu"] = reversals[2]  # outer upper xpoint
+    xpoint["ol"] = reversals[-1] - 1  # outer lower xpoint
+    xpoint["ou"] = reversals[-3]  # outer upper xpoint
 
     target["il"] = 0
-    target["iu"] = reversals[3]+1
-    target["ou"] = reversals[4]-1
+    target["iu"] = reversals[3] + 1
+    target["ou"] = reversals[4] - 1
     target["ol"] = len(gradR)
-    
+
     if diagnostic_plot is True:
-        fig, ax = plt.subplots(dpi = 150)
+        fig, ax = plt.subplots(dpi=150)
         ax.set_aspect("equal")
-        ax.scatter(full["R"], full["Z"], s = 5)
+        ax.scatter(full["R"], full["Z"], s=5)
         for i in reversals:
-            ax.scatter(full["R"][i], full["Z"][i], color = "red", marker = "*", s = 10)
-        
-        
+            ax.scatter(full["R"][i], full["Z"][i], color="red", marker="*", s=10)
 
     # Define start and end for each segment, going clockwise from bottom left.
-    start = dict(); end = dict()
+    start = dict()
+    end = dict()
     start["il"] = target["il"]
-    start["iu"] = imp-1
+    start["iu"] = imp - 1
     start["ou"] = target["ou"]
-    start["ol"] = omp-1
+    start["ol"] = omp - 1
 
-    end["il"] = imp+1
+    end["il"] = imp + 1
     end["iu"] = target["iu"]
-    end["ou"] = omp+1
+    end["ou"] = omp + 1
     end["ol"] = target["ol"]
 
     # Extract the four SOLs using their starts and ends.
-    
-    sol = defaultdict(dict) # Dict of parameters in each SOL side. sol[param][side]
+
+    sol = defaultdict(dict)  # Dict of parameters in each SOL side. sol[param][side]
 
     for param in full.keys():
         for side in ["il", "iu", "ou", "ol"]:
             sol[param][side] = full[param][start[side] : end[side]]
 
-    # Invert inner lower and outer upper so that all SOLs 
+    # Invert inner lower and outer upper so that all SOLs
     # are consistent and start at midplane (needs to be done under this convention to work)
     # This can be later reversed to start at target by input flag "convention=target_to_midplane"
     for param in full.keys():
         sol[param]["il"] = sol[param]["il"][::-1]
         sol[param]["ou"] = sol[param]["ou"][::-1]
 
-
     """------INTERPOLATION"""
-    
-    path_actual = dict() # Spol
-    path_grid = dict() # grid to interpolate over
-    interp = defaultdict(dict) # dict of interpolators
-    data = defaultdict(dict) # final parameters
-     
+
+    path_actual = dict()  # Spol
+    path_grid = dict()  # grid to interpolate over
+    interp = defaultdict(dict)  # dict of interpolators
+    data = defaultdict(dict)  # final parameters
+
     for side in ["ol", "il", "ou", "iu"]:
         path_actual[side] = returnll(sol["R"][side], sol["Z"][side])
 
@@ -154,21 +155,22 @@ def unpackConfigurationMK(File,
     # Offset the actual paths so they're zero at the actual midplane inbetween the two cells there
     # Then make grids to interpolate on that start at zero and end at the path end
     for side in ["iu", "il"]:
-        path_actual[side] -= imp_len/2
+        path_actual[side] -= imp_len / 2
         # path_grid[side] = np.linspace(0, np.amax(path_actual[side]), resolution)
 
     for side in ["ou", "ol"]:
-        path_actual[side] -= omp_len/2
+        path_actual[side] -= omp_len / 2
         # path_grid[side] = np.linspace(0, np.amax(path_actual[side]), resolution)
 
     # Create interpolators and apply them
     for side in ["ol", "il", "ou", "iu"]:
-        
         # Regular linearly spaced grid (linear in poloidal space)
         path_grid[side] = np.linspace(0, np.amax(path_actual[side]), resolution)
-        
+
         for param in full.keys():
-            interp[side][param] = interpolate.interp1d(path_actual[side], sol[param][side], kind = "cubic")
+            interp[side][param] = interpolate.interp1d(
+                path_actual[side], sol[param][side], kind="cubic"
+            )
             data[side][param] = interp[side][param](path_grid[side])
 
     # Find Xpoints again in the interpolated grid using gradient sign change
@@ -176,120 +178,148 @@ def unpackConfigurationMK(File,
     for side in ["ol", "il", "ou", "iu"]:
         gradientR = np.gradient(data[side]["R"])
 
-        for i in range(len(gradientR)-1):
-            if np.sign(gradientR[i]) != np.sign(gradientR[i+1]):
-                data[side]["Xpoint"] = i+1
-
-            
+        for i in range(len(gradientR) - 1):
+            if np.sign(gradientR[i]) != np.sign(gradientR[i + 1]):
+                data[side]["Xpoint"] = i + 1
 
     """------OTHER CALCULATIONS"""
-    
-    zl = dict() # Z space path
-    Bx = dict() # Btot at Xpoint
-    polLengthArray = dict() # another poloidal distance path but now of the interpolated SOLs
-    S = dict() # Real distance path as opposed to poloidal distance
-    
+
+    zl = dict()  # Z space path
+    Bx = dict()  # Btot at Xpoint
+    polLengthArray = (
+        dict()
+    )  # another poloidal distance path but now of the interpolated SOLs
+    S = dict()  # Real distance path as opposed to poloidal distance
+
     for side in ["ol", "il", "ou", "iu"]:
         d = data[side]
-        
+
         # Reverse data if we want it target to midplane
         if convention == "target_to_midplane":
             for param in (x for x in d.keys() if x not in ["Xpoint"]):
                 d[param] = d[param][::-1]
             d["Xpoint"] = len(d["R"]) - d["Xpoint"] - 1
-            
+
         # Make B fields positive if required
         if absolute_B:
             d["Btot"] = abs(d["Btot"])
             d["Bpol"] = abs(d["Bpol"])
-        
+
         # Poloidal distance
         d["Spol"] = np.array(returnll(d["R"], d["Z"]))
         d["S"] = np.array(returnS(d["R"], d["Z"], d["Btot"], d["Bpol"]))
-        
+
         # Z space distance (combined parallel and flux expansion, see Lipschultz 2016)
-        d["zl"] = np.array(returnzl(d["R"], d["Z"], d["Btot"][d["Xpoint"]], np.absolute(d["Bpol"])))
-        
-        
-    """------DIAGNOSTIC PLOT""" 
-    
+        d["zl"] = np.array(
+            returnzl(d["R"], d["Z"], d["Btot"][d["Xpoint"]], np.absolute(d["Bpol"]))
+        )
+
+    """------DIAGNOSTIC PLOT"""
+
     # Plot the four divertor SOLs and corresponding Xpoints
     if diagnostic_plot == True:
-        
-        fig, ax = plt.subplots(1,4, figsize = (18,4))
+        fig, ax = plt.subplots(1, 4, figsize=(18, 4))
 
         xparam = "S"
         yparam = "Btot"
 
         for i, side in enumerate(["iu", "il", "ou", "ol"]):
-
             d = data[side]
             Xpoint = d["Xpoint"]
             ax[i].set_title(side)
-            ax[i].plot(d[xparam], d[yparam], marker = "o", color = "None", markerfacecolor = "None", markeredgecolor = "purple", alpha = 0.5, zorder = 0)
-            ax[i].scatter(d[xparam][Xpoint], d[yparam][Xpoint], marker = "o", s = 50, edgecolor = "black", color = "yellow", label = "Xpoint", zorder = 1)
-            ax[i].legend(); ax[i].grid()
-            ax[i].set_xlabel(xparam); ax[i].set_ylabel(yparam)
-        
+            ax[i].plot(
+                d[xparam],
+                d[yparam],
+                marker="o",
+                color="None",
+                markerfacecolor="None",
+                markeredgecolor="purple",
+                alpha=0.5,
+                zorder=0,
+            )
+            ax[i].scatter(
+                d[xparam][Xpoint],
+                d[yparam][Xpoint],
+                marker="o",
+                s=50,
+                edgecolor="black",
+                color="yellow",
+                label="Xpoint",
+                zorder=1,
+            )
+            ax[i].legend()
+            ax[i].grid()
+            ax[i].set_xlabel(xparam)
+            ax[i].set_ylabel(yparam)
+
         fig.show()
-        
-    """------OUTPUT"""    
+
+    """------OUTPUT"""
 
     # Pack into a Profile class
     profiles = {}
     for i, side in enumerate(["iu", "il", "ou", "ol"]):
         d = data[side]
-        profiles[side] = Profile(d["R"], d["Z"], d["Xpoint"], d["Btot"], d["Bpol"], d["S"], d["Spol"], name = side)
-    
+        profiles[side] = Profile(
+            d["R"],
+            d["Z"],
+            d["Xpoint"],
+            d["Btot"],
+            d["Bpol"],
+            d["S"],
+            d["Spol"],
+            name=side,
+        )
+
     # Output by geometry type
     if Type != "box":
         return profiles[Type]
     else:
         print("Slab geometry not supported yet")
-        
-        
-    
-def returnll(R,Z):
-    #return the poloidal distances from the target for a given configuration
+
+
+def returnll(R, Z):
+    # return the poloidal distances from the target for a given configuration
     PrevR = R[0]
     ll = []
     currentl = 0
     PrevZ = Z[0]
     for i in range(len(R)):
-        dl = np.sqrt((PrevR-R[i])**2 + (PrevZ-Z[i])**2)
-        currentl = currentl+ dl
+        dl = np.sqrt((PrevR - R[i]) ** 2 + (PrevZ - Z[i]) ** 2)
+        currentl = currentl + dl
         ll.append(currentl)
         PrevR = R[i]
         PrevZ = Z[i]
     return ll
 
-def returnS(R,Z,B,Bpol):
-    #return the real total distances from the target for a given configuration
+
+def returnS(R, Z, B, Bpol):
+    # return the real total distances from the target for a given configuration
     PrevR = R[0]
     s = []
     currents = 0
     PrevZ = Z[0]
     for i in range(len(R)):
-        dl = np.sqrt((PrevR-R[i])**2 + (PrevZ-Z[i])**2)
-        ds = dl*np.abs(B[i])/np.abs(Bpol[i])
-        currents = currents+ ds
+        dl = np.sqrt((PrevR - R[i]) ** 2 + (PrevZ - Z[i]) ** 2)
+        ds = dl * np.abs(B[i]) / np.abs(Bpol[i])
+        currents = currents + ds
         s.append(currents)
         PrevR = R[i]
         PrevZ = Z[i]
     return s
 
-def returnzl(R,Z, BX, Bpol):
+
+def returnzl(R, Z, BX, Bpol):
     # return the distance in z from the target for a given configuration
     PrevR = R[0]
     PrevZ = Z[0]
     CurrentZ = 0
     zl = []
     for i in range(len(R)):
-        dl = np.sqrt((PrevR-R[i])**2 + (PrevZ-Z[i])**2)
-        dz = dl*BX/(Bpol[i])
-        CurrentZ = CurrentZ+ dz
+        dl = np.sqrt((PrevR - R[i]) ** 2 + (PrevZ - Z[i]) ** 2)
+        dz = dl * BX / (Bpol[i])
+        CurrentZ = CurrentZ + dz
         zl.append(CurrentZ)
         PrevR = R[i]
         PrevZ = Z[i]
     return zl
-
