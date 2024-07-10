PlasmaFAIR/DLS-model

General tidy-up of `Analytic_DLS`
- fix docstring
- use a constant for `epsabs`
- initialise `Q` in declaration
- use `sqrt` instead of `x**(1/2)`
- remove unused argument from `integrand`
- use `Tu += ...` instead of `Tu = Tu + ...`

src/fusiondls/Analytic_DLS.py
@@ -1,12 +1,14 @@
 import numpy as np
-from scipy import interpolate
+from scipy.interpolate import interp1d
 from scipy.integrate import quad, trapezoid
 
 from .AnalyticCoolingCurves import LfuncN
 
 
 def CfInt(spar, B_field, sx, L, sh=0, kappa1=2500):
-    """
+    """Calculate the control parameter required for a detachment front
+    at parallel position ``sh``
+
     spar: array, m
         Array of S parallel
     B_field: array, T
@@ -14,39 +16,31 @@ def CfInt(spar, B_field, sx, L, sh=0, kappa1=2500):
     sx: float, m
         Position of detachment front in the parallel
     L: float, m
-        Connection length in  the parallel
+        Connection length in the parallel
     sh: float, m
         Parallel front position
     kappa1: float, W/m^2/K^7/2
         Electron thermal conductivity
 
     """
-    """function which returns the control parameter required for a detachment front at parallel position sh"""
-    B_field = interpolate.interp1d(
-        spar, B_field, kind="cubic", fill_value="extrapolate"
-    )
+
+    B_field = interp1d(spar, B_field, kind="cubic", fill_value="extrapolate")
     # calculate Tu/qpll**(2/7) by integrating over heat flux density
-    Tu = quad(integrand, sh, sx, args=(sx, L, B_field), epsabs=0.0000000000000000001)[0]
+    epsabs = 1e-18
+    Tu = quad(integrand, sh, sx, args=(sx, B_field), epsabs=epsabs)[0]
     if sx < L:
-        Tu = (
-            Tu
-            + quad(
-                integrand2, sx, L, args=(sx, L, B_field), epsabs=0.0000000000000000001
-            )[0]
-        )
+        Tu += quad(integrand2, sx, L, args=(sx, L, B_field), epsabs=epsabs)[0]
     Tu = (Tu * 7 / (2 * kappa1)) ** (-2 / 7)
     # account for flux expansion effects on heat flux density
     Cf = Tu * B_field(sh) / B_field(sx)
     # account for constants related to total impurity radiation
-    Q = []
     T = np.linspace(0, 100, 1000)
-    for t in T:
-        Q.append(LfuncN(t))
-    C0 = (2 * kappa1 * trapezoid(Q * T ** (1 / 2), T)) ** (-1 / 2)
+    Q = np.array([LfuncN(t) for t in T])
+    C0 = 1.0 / np.sqrt(2 * kappa1 * trapezoid(Q * np.sqrt(T), T))
     return Cf * C0
 
 
-def integrand(s, sx, L, B_field):
+def integrand(s, sx, B_field):
     return B_field(s) / B_field(sx)
 
 

FusionDLS/FusionDLS

src/fusiondls/Analytic_DLS.py
@@ -1,12 +1,13 @@
 import numpy as np
-from scipy import interpolate
+from scipy.interpolate import interp1d
 from scipy.integrate import quad, trapezoid
 
 from .AnalyticCoolingCurves import LfuncN
 
 
 def CfInt(spar, B_field, sx, L, sh=0, kappa1=2500):
-    """Calculate the control parameter required for a detachment front at parallel position ``sh``
+    """Calculate the control parameter required for a detachment front
+    at parallel position ``sh``
 
     Parameters
     ----------
@@ -17,7 +18,7 @@ def CfInt(spar, B_field, sx, L, sh=0, kappa1=2500):
     sx: float, m
         Position of detachment front in the parallel
     L: float, m
-        Connection length in  the parallel
+        Connection length in the parallel
     sh: float, m
         Parallel front position
     kappa1: float, W/m^2/K^7/2
@@ -25,28 +26,23 @@ def CfInt(spar, B_field, sx, L, sh=0, kappa1=2500):
 
     """
 
-    B_field = interpolate.interp1d(
-        spar, B_field, kind="cubic", fill_value="extrapolate"
-    )
+    B_field = interp1d(spar, B_field, kind="cubic", fill_value="extrapolate")
     # calculate Tu/qpll**(2/7) by integrating over heat flux density
-    Tu = quad(_integrand, sh, sx, args=(sx, L, B_field), epsabs=0.0000000000000000001)[
-        0
-    ]
+    epsabs = 1e-18
+    Tu = quad(_integrand, sh, sx, args=(sx, B_field), epsabs=epsabs)[0]
     if sx < L:
-        Tu += quad(_integrand2, sx, L, args=(sx, L, B_field), epsabs=1e-19)[0]
+        Tu += quad(_integrand2, sx, L, args=(sx, L, B_field), epsabs=epsabs)[0]
     Tu = (Tu * 7 / (2 * kappa1)) ** (-2 / 7)
     # account for flux expansion effects on heat flux density
     Cf = Tu * B_field(sh) / B_field(sx)
     # account for constants related to total impurity radiation
-    Q = []
     T = np.linspace(0, 100, 1000)
-    for t in T:
-        Q.append(LfuncN(t))
-    C0 = (2 * kappa1 * trapezoid(Q * T ** (1 / 2), T)) ** (-1 / 2)
+    Q = np.array([LfuncN(t) for t in T])
+    C0 = 1.0 / np.sqrt(2 * kappa1 * trapezoid(Q * np.sqrt(T), T))
     return Cf * C0
 
 
-def _integrand(s, sx, L, B_field):
+def _integrand(s, sx, B_field):
     return B_field(s) / B_field(sx)
 
 