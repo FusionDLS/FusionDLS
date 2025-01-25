629fec663521c066e3396ccfd1e5e1887414b98f
Mike Kryjak <mikekryjak@gmail.com>
2025-01-25 20:07:40 +0000

    Take out post-processing from solver
    This is now done in FrontLocationScan

--- src/fusiondls/solver.py
+++ src/fusiondls/solver.py
@@ -158,13 +158,7 @@ class SimulationOutput(Mapping):
     spar_onset: int
     spol_onset: int
     splot: FloatArray
-    crel: FloatArray
-    cvar_trim: FloatArray
-    crel_trim: FloatArray
     threshold: float
-    window: float
-    window_frac: float
-    window_ratio: float
     inputs: SimulationInputs
     geometry: MagneticGeometry
     state: SimulationState
@@ -183,16 +177,6 @@ class SimulationOutput(Mapping):
     Xpoints: list[FloatArray]
     Wradials: list[FloatArray]
     logs: dict
-    spar_onset: int
-    spol_onset: int
-    splot: list[FloatArray]
-    crel: list[FloatArray]
-    cvar_trim: list[FloatArray]
-    crel_trim: list[FloatArray]
-    threshold: float
-    window: float
-    window_frac: float
-    window_ratio: float
     inputs: SimulationInputs
     geometry: MagneticGeometry
     state: SimulationState
@@ -476,70 +460,10 @@ def run_dls(
 
     output["logs"] = st.log  # Append log with all front positions
 
-    """------COLLECT RESULTS------"""
-    if len(inputs.SparRange) > 1:
-        # Here we calculate things like window, threshold etc from a whole scan.
-
-        # Relative control variable:
-        cvar_list = np.array(output["cvar"])
-        crel_list = cvar_list / cvar_list[0]
-
-        # S parallel and poloidal locations of each front location (for plotting against cvar/crel):
-        splot = output["Splot"]
-        spolplot = output["SpolPlot"]
-
-        # Trim any unstable detachment (negative gradient) region for post-processing reasons
-        crel_list_trim = crel_list.copy()
-        cvar_list_trim = cvar_list.copy()
-
-        # Find values on either side of C = 1 and interpolate onto 1
-        if len(crel_list) > 1:
-            for i in range(len(crel_list) - 1):
-                if np.sign(crel_list[i] - 1) != np.sign(crel_list[i + 1] - 1) and i > 0:
-                    interp_par = interpolate.interp1d(
-                        [crel_list[i], crel_list[i + 1]], [splot[i], splot[i + 1]]
-                    )
-                    interp_pol = interpolate.interp1d(
-                        [crel_list[i], crel_list[i + 1]], [spolplot[i], spolplot[i + 1]]
-                    )
-
-                    spar_onset = float(interp_par(1))
-                    spol_onset = float(interp_pol(1))
-                    break
-                if i == len(crel_list) - 2:
-                    spar_onset = 0
-                    spol_onset = 0
-
-            output["spar_onset"] = spar_onset
-            output["spol_onset"] = spol_onset
-
-            grad = np.gradient(crel_list)
-            for i, _val in enumerate(grad):
-                if i > 0 and np.sign(_val) != np.sign(grad[i - 1]):
-                    crel_list_trim[:i] = np.nan
-                    cvar_list_trim[:i] = np.nan
-
-        # Pack things into the output dictionary.
-
-        output["splot"] = splot
-        output["cvar"] = cvar_list
-        output["crel"] = crel_list
-        output["cvar_trim"] = cvar_list_trim
-        output["crel_trim"] = crel_list_trim
-        output["threshold"] = cvar_list[0]
-        # Ct
-        output["window"] = cvar_list[-1] - cvar_list[0]  # Cx - Ct
-        output["window_frac"] = output["window"] / output["threshold"]  # (Cx - Ct) / Ct
-        output["window_ratio"] = cvar_list[-1] / cvar_list[0]  # Cx / Ct
-
-    elif len(inputs.SparRange) == 1:
-        output["crel"] = 1
-        output["threshold"] = st.cvar
-
     t1 = timer()
-
     print(f"Complete in {t1 - t0:.1f} seconds")
 
+    # return output
     return SimulationOutput(inputs=inputs, geometry=geometry, state=st, **output)
 
 
